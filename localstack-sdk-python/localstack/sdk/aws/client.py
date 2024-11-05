82f3f82b9bfcf2b30d0759c6c600f79288a13ee6
Giovanni Grano <me@giograno.com>
2024-11-05 10:02:26 +0100

    add operations to the client and some tests

--- localstack-sdk-python/localstack/sdk/aws/client.py
+++ localstack-sdk-python/localstack/sdk/aws/client.py
@@ -2,7 +2,7 @@ import json
 
 from localstack.clients import BaseClient
 from localstack.sdk.api.aws_api import AwsApi
-from localstack.sdk.models import Message
+from localstack.sdk.models import Message, SesSentEmail
 
 
 def _from_sqs_query_to_json(xml_dict: dict) -> list[Message]:
@@ -35,6 +35,10 @@ class AWSClient(BaseClient):
         super().__init__(**kwargs)
         self._client = AwsApi(self._api_client)
 
+    ########
+    # SQS
+    ########
+
     def list_sqs_messages(self, account_id: str, region: str, queue_name: str) -> list[Message]:
         response = self._client.list_sqs_messages_with_http_info(
             account_id=account_id, region=region, queue_name=queue_name
@@ -44,6 +48,19 @@ class AWSClient(BaseClient):
     def list_sqs_messages_from_queue_url(self, queue_url) -> list[Message]:
         response = self._client.list_all_sqs_messages_with_http_info(queue_url=queue_url)
         return _from_sqs_query_to_json(json.loads(response.raw_data))
+
+    ########
+    # SES
+    ########
+
+    def get_ses_messages(
+        self, id_filter: str | None = None, email_filter: str | None = None
+    ) -> list[SesSentEmail]:
+        response = self._client.get_ses_messages(id=id_filter, email=email_filter)
+        return response.messages
+
+    def discard_ses_messages(self, id_filter: str | None = None):
+        return self._client.discard_ses_messages(id=id_filter)
 
 
 def get_default(**args) -> AwsApi:
--- tests/integration/test_aws.py
+++ tests/integration/test_aws.py
@@ -5,6 +5,20 @@ import boto3
 import localstack.sdk.aws
 from tests.utils import short_uid
 
+SAMPLE_SIMPLE_EMAIL = {
+    "Subject": {
+        "Data": "SOME_SUBJECT",
+    },
+    "Body": {
+        "Text": {
+            "Data": "SOME_MESSAGE",
+        },
+        "Html": {
+            "Data": "<p>SOME_HTML</p>",
+        },
+    },
+}
+
 
 class TestLocalStackAWS:
     client = localstack.sdk.aws.AWSClient()
@@ -70,3 +84,59 @@ class TestLocalStackAWS:
             account_id="000000000000", region="us-east-1", queue_name=queue_name
         )
         assert messages == []
+
+    def test_get_and_discard_ses_messages(self):
+        aws_client = boto3.client(
+            "ses",
+            endpoint_url=self.client.configuration.host,
+            region_name="us-east-1",
+            aws_access_key_id="test",
+            aws_secret_access_key="test",
+        )
+
+        email = f"user-{short_uid()}@example.com"
+        aws_client.verify_email_address(EmailAddress=email)
+
+        message1 = aws_client.send_email(
+            Source=email,
+            Message=SAMPLE_SIMPLE_EMAIL,
+            Destination={
+                "ToAddresses": ["success@example.com"],
+            },
+        )
+        message1_id = message1["MessageId"]
+
+        # Send a raw message
+        raw_message_data = f"From: {email}\nTo: recipient@example.com\nSubject: test\n\nThis is the message body.\n\n"
+        message2 = aws_client.send_raw_email(RawMessage={"Data": raw_message_data})
+        message2_id = message2["MessageId"]
+
+        # filter by message id
+        messages = self.client.get_ses_messages(id_filter=message1_id)
+        assert len(messages) == 1
+        assert messages[0].id == message1_id
+        assert messages[0].subject == "SOME_SUBJECT"
+        assert messages[0].body.html_part == "<p>SOME_HTML</p>"
+        assert messages[0].body.text_part == "SOME_MESSAGE"
+
+        messages = self.client.get_ses_messages(id_filter=message2_id)
+        assert len(messages) == 1
+
+        # filter by email body
+        messages = self.client.get_ses_messages(email_filter="none@example.com")
+        assert len(messages) == 0
+        messages = self.client.get_ses_messages(email_filter=email)
+        assert len(messages) == 2
+
+        # no filter
+        messages = self.client.get_ses_messages()
+        assert len(messages) == 2
+
+        # discard messages
+        self.client.discard_ses_messages(id_filter=message1_id)
+        messages = self.client.get_ses_messages(id_filter=message2_id)
+        assert len(messages) == 1
+        assert messages[0].id == message2_id
+
+        self.client.discard_ses_messages()
+        assert not self.client.get_ses_messages()
