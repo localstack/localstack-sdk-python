e60cd83a78b5d5f2f24aa73f96603e086c9beea7
Giovanni Grano <me@giograno.com>
2024-11-21 18:04:35 -0500

    more docstrings

--- localstack-sdk-python/localstack/sdk/chaos/managers.py
+++ localstack-sdk-python/localstack/sdk/chaos/managers.py
@@ -6,6 +6,11 @@ from localstack.sdk.models import FaultRule
 
 @contextmanager
 def fault_configuration(fault_rules: list[FaultRule]):
+    """
+    This is a context manager that temporarily applies a given set of fault rules.
+    :param fault_rules: a list of FaultRule to be applied.
+    :return: None
+    """
     client = get_default()
     try:
         client.set_fault_rules(fault_rules=fault_rules)
--- localstack-sdk-python/localstack/sdk/state/client.py
+++ localstack-sdk-python/localstack/sdk/state/client.py
@@ -3,9 +3,17 @@ from localstack.sdk.api import StateApi
 
 
 class StateClient(BaseClient):
+    """
+    Initializes a client to handle LocalStack's state.
+    """
+
     def __init__(self, **args) -> None:
         super().__init__(**args)
         self._client = StateApi(self._api_client)
 
     def reset_state(self) -> None:
+        """
+        Resets the state of LocalStack for all running services.
+        :return: None
+        """
         self._client.localstack_state_reset_post_0()
