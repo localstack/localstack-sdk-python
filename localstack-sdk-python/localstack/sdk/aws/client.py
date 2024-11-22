ef87334ea12e6817dad620f065019c35b0d6cbf9
Giovanni Grano <me@giograno.com>
2024-11-22 10:25:58 -0500

    fix indentation in docstrings

--- localstack-sdk-python/localstack/sdk/aws/client.py
+++ localstack-sdk-python/localstack/sdk/aws/client.py
@@ -53,6 +53,7 @@ class AWSClient(BaseClient):
     def list_sqs_messages(self, account_id: str, region: str, queue_name: str) -> list[Message]:
         """
         Lists all the SQS messages in a given queue in a specific account id and region, without any side effect.
+
         :param account_id: the account id of the queue
         :param region: the region of the queue
         :param queue_name: the name of the queue
@@ -66,6 +67,7 @@ class AWSClient(BaseClient):
     def list_sqs_messages_from_queue_url(self, queue_url: str) -> list[Message]:
         """
         Lists all the SQS messages in a given queue, without any side effect.
+
         :param queue_url: the URL of the queue
         :return: the list of messages in the queue
         """
@@ -81,6 +83,7 @@ class AWSClient(BaseClient):
     ) -> list[SesSentEmail]:
         """
         Returns all the in-memory saved SES messages. They can be filtered by message ID and/or message source.
+
         :param id_filter: the message id used as filter for the SES messages
         :param email_filter: the message source filter
         :return: a list of email sent with SES
@@ -91,6 +94,7 @@ class AWSClient(BaseClient):
     def discard_ses_messages(self, id_filter: str | None = None) -> None:
         """
         Clears all SES messages. An ID filter can be provided to delete only a specific message.
+
         :param id_filter: the id filter
         :return: None
         """
@@ -108,6 +112,7 @@ class AWSClient(BaseClient):
     ) -> SNSSMSMessagesResponse:
         """
         Returns all SMS messages published to a phone number.
+
         :param phone_number: the phone number to which the messages have been published. If not specified, all messages
             are returned.
         :param account_id: the AWS Account ID from which the messages have been published. '000000000000' by default
@@ -126,6 +131,7 @@ class AWSClient(BaseClient):
     ) -> None:
         """
         Discards all SMS messages published to a phone number.
+
         :param phone_number: the phone number to which the messages have been published. If not specified, all messages
             are deleted.
         :param account_id: the AWS Account ID from which the messages have been published. '000000000000' by default
@@ -144,6 +150,7 @@ class AWSClient(BaseClient):
     ) -> SNSPlatformEndpointResponse:
         """
         Returns all the messages published to a platform endpoint.
+
         :param endpoint_arn: the ARN to which the messages have been published. If not specified, will return all the
             messages.
         :param account_id: the AWS Account ID from which the messages have been published. 000000000000 if not specified
@@ -162,6 +169,7 @@ class AWSClient(BaseClient):
     ) -> None:
         """
         Discards all the messaged published to a platform endpoint.
+
         :param endpoint_arn: the ARN to which the messages have been published. If not specified, will discard all the
             messages.
         :param account_id: the AWS Account ID from which the messages have been published. 000000000000 if not specified
