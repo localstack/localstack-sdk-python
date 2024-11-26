ac81a9ec0ca5d10d4288d517b7781250fc8747f5
Giovanni Grano <me@giograno.com>
2024-11-26 12:59:46 -0500

    Adding docstrings and introducing sphinx draft (#12)

---
+++ docs/conf.py
@@ -0,0 +1,59 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../localstack-sdk-python/localstack"))
+
+
+def _get_version() -> str:
+    with open(os.path.abspath("../VERSION"), "r") as f:
+        version = f.read()
+        return version
+
+
+project = "LocalStack Python SDK"
+copyright = "2024, LocalStack Team"
+author = "LocalStack Team"
+release = _get_version()
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# enable auto-doc extension
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+html_sidebars = {
+    "**": [
+        "about.html",
+        "searchfield.html",
+        "navigation.html",
+    ]
+}
+
+html_theme_options = {
+    "extra_nav_links": {
+        "Source": "https://github.com/localstack/localstack-sdk-python",
+        "Issues": "https://github.com/localstack/localstack-sdk-python/issues",
+        "PyPI": "https://pypi.org/project/localstack-sdk-python/",
+    },
+    "description": "Python SDK to interact with LocalStack developer endpoints",
+    "github_user": "localstack",
+    "github_repo": "localstack-sdk-python",
+}
--- localstack-sdk-python/localstack/sdk/aws/client.py
+++ localstack-sdk-python/localstack/sdk/aws/client.py
@@ -1,7 +1,7 @@
 import json
 
-from localstack.clients import BaseClient
 from localstack.sdk.api.aws_api import AwsApi
+from localstack.sdk.clients import BaseClient
 from localstack.sdk.models import (
     Message,
     SesSentEmail,
@@ -36,6 +36,12 @@ def _from_sqs_query_to_json(xml_dict: dict) -> list[Message]:
 
 
 class AWSClient(BaseClient):
+    """
+    The client to interact with all the LocalStack's AWS endpoints.
+    These endpoints offer specific features in addition to the ones offered by the AWS services. For instance,
+    access all the messages withing a SQS without the side effect of deleting them.
+    """
+
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self._client = AwsApi(self._api_client)
@@ -45,12 +51,26 @@ class AWSClient(BaseClient):
     ########
 
     def list_sqs_messages(self, account_id: str, region: str, queue_name: str) -> list[Message]:
+        """
+        Lists all the SQS messages in a given queue in a specific account id and region, without any side effect.
+
+        :param account_id: the account id of the queue
+        :param region: the region of the queue
+        :param queue_name: the name of the queue
+        :return: the list of messages in the queue
+        """
         response = self._client.list_sqs_messages_with_http_info(
             account_id=account_id, region=region, queue_name=queue_name
         )
         return _from_sqs_query_to_json(json.loads(response.raw_data))
 
-    def list_sqs_messages_from_queue_url(self, queue_url) -> list[Message]:
+    def list_sqs_messages_from_queue_url(self, queue_url: str) -> list[Message]:
+        """
+        Lists all the SQS messages in a given queue, without any side effect.
+
+        :param queue_url: the URL of the queue
+        :return: the list of messages in the queue
+        """
         response = self._client.list_all_sqs_messages_with_http_info(queue_url=queue_url)
         return _from_sqs_query_to_json(json.loads(response.raw_data))
 
@@ -61,10 +81,23 @@ class AWSClient(BaseClient):
     def get_ses_messages(
         self, id_filter: str | None = None, email_filter: str | None = None
     ) -> list[SesSentEmail]:
+        """
+        Returns all the in-memory saved SES messages. They can be filtered by message ID and/or message source.
+
+        :param id_filter: the message id used as filter for the SES messages
+        :param email_filter: the message source filter
+        :return: a list of email sent with SES
+        """
         response = self._client.get_ses_messages(id=id_filter, email=email_filter)
         return response.messages
 
     def discard_ses_messages(self, id_filter: str | None = None) -> None:
+        """
+        Clears all SES messages. An ID filter can be provided to delete only a specific message.
+
+        :param id_filter: the id filter
+        :return: None
+        """
         return self._client.discard_ses_messages(id=id_filter)
 
     ########
@@ -77,6 +110,15 @@ class AWSClient(BaseClient):
         account_id: str = "000000000000",
         region: str = "us-east-1",
     ) -> SNSSMSMessagesResponse:
+        """
+        Returns all SMS messages published to a phone number.
+
+        :param phone_number: the phone number to which the messages have been published. If not specified, all messages
+            are returned.
+        :param account_id: the AWS Account ID from which the messages have been published. '000000000000' by default
+        :param region:  the AWS region from which the messages have been published. us-east-1 by default
+        :return:
+        """
         return self._client.get_sns_sms_messages(
             phone_number=phone_number, account_id=account_id, region=region
         )
@@ -87,6 +129,15 @@ class AWSClient(BaseClient):
         account_id: str = "000000000000",
         region: str = "us-east-1",
     ) -> None:
+        """
+        Discards all SMS messages published to a phone number.
+
+        :param phone_number: the phone number to which the messages have been published. If not specified, all messages
+            are deleted.
+        :param account_id: the AWS Account ID from which the messages have been published. '000000000000' by default
+        :param region:  the AWS region from which the messages have been published. us-east-1 by default
+        :return: None
+        """
         return self._client.discard_sns_sms_messages(
             phone_number=phone_number, account_id=account_id, region=region
         )
@@ -97,6 +148,15 @@ class AWSClient(BaseClient):
         account_id: str = "000000000000",
         region: str = "us-east-1",
     ) -> SNSPlatformEndpointResponse:
+        """
+        Returns all the messages published to a platform endpoint.
+
+        :param endpoint_arn: the ARN to which the messages have been published. If not specified, will return all the
+            messages.
+        :param account_id: the AWS Account ID from which the messages have been published. 000000000000 if not specified
+        :param region: the AWS region from which the messages have been published. us-east-1 by default
+        :return: a response with the list of messages and the queried region
+        """
         return self._client.get_sns_endpoint_messages(
             endpoint_arn=endpoint_arn, account_id=account_id, region=region
         )
@@ -107,6 +167,15 @@ class AWSClient(BaseClient):
         account_id: str = "000000000000",
         region: str = "us-east-1",
     ) -> None:
+        """
+        Discards all the messaged published to a platform endpoint.
+
+        :param endpoint_arn: the ARN to which the messages have been published. If not specified, will discard all the
+            messages.
+        :param account_id: the AWS Account ID from which the messages have been published. 000000000000 if not specified
+        :param region: the AWS region from which the messages have been published. us-east-1 by default
+        :return: None
+        """
         return self._client.discard_sns_endpoint_messages(
             endpoint_arn=endpoint_arn, account_id=account_id, region=region
         )
